da4b4efc4e4f831b3558206d68841f0b7901a748
orlann <orlann@ukr.net>
2020-11-18 04:02:11 +0200

    finished lab 5.3

---
+++ UnitTest5/UnitTest5.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 5.3/5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest5
+{
+	TEST_CLASS(UnitTest5)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double actualResult;
+			actualResult = f(5.0);
+			double expectedResult = -0.417363;
+			Assert::AreEqual(to_string(actualResult), to_string(expectedResult));
+
+		}
+	};
+}
